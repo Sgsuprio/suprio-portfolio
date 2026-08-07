a5ab38f0357e10cefddb093b9787db62868ce2d1
Suprio <91708978+Sgsuprio@users.noreply.github.com>
2026-08-07 22:56:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("open");
     navLinks.classList.toggle("active");
-
-    if (navLinks.classList.contains("active")) {
-        menuBtn.innerHTML = "✕";
-    } else {
-        menuBtn.innerHTML = "☰";
-    }
 });
 
-document.querySelectorAll(".nav-links a").forEach(link => {
-    link.addEventListener("click", () => {
+document.querySelectorAll(".nav-links a").forEach(link=>{
+    link.addEventListener("click",()=>{
+        menuBtn.classList.remove("open");
         navLinks.classList.remove("active");
-        menuBtn.innerHTML = "☰";
     });
 });
